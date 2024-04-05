01368616c442a6a1208b26a81f21e533b14c6fc3
Your Name <you@example.co>
2024-04-05 11:23:37 +0200

    añadir nuevo gatito

--- main.js
+++ main.js
@@ -1,6 +1,62 @@
 'use strict';
 
-const kittenOne = document.querySelector (".card");
-kittenOne.innerHTML += "<li> card </li>";
-console.log(kittenOne)
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://dev.adalab.es/gato-siames.webp"
+    alt="gatito"
+  />
+  <h3 class="card_title">Anastacio</h3>
+  <h4 class="card_race">Siamés</h4>
+  <p class="card_description">
+            Porte elegante, su patrón de color tan característico y sus ojos
+            de un azul intenso, pero su historia se remonta a Asía al menos
+            hace 500 años, donde tuvo su origen muy posiblemente.
+   </p>
+</article>
+</li>`;
+const kittenTwo = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://dev.adalab.es/sphynx-gato.webp"alt="sphynx-cat"
+    alt="gatito"
+  />
+  <h3 class="card_title">Fiona</h3>
+  <h4 class="card_race">Sphynx</h4>
+  <p class="card_description">
+  Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+  hasta con pinta de alienígena han llegado a definir a esta raza
+  gatuna que se caracteriza por la «ausencia» de pelo.
+   </p>
+</article>
+</li>`;
+const kittenThree = `<li class="card">
+<img
+              class="card_img"
+              src="https://dev.adalab.es/maine-coon-cat.webp"
+              alt="maine-coon-cat"
+            />
+            <h3 class="card_title">Cielo</h3>
+            <h4 class="card_race">Maine Coon</h4>
+            <p class="card_description">
+              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+              bella mirada se ha convertido en una de sus señas de identidad.
+              Sus ojos son grandes y las orejas resultan largas y en punta.</p>
+              </li>`;
 
+              document.querySelector (".js-list").innerHTML = kittenOne + kittenTwo + kittenThree;
+
+const menuAdd =document.querySelector(".js-btn-add");
+const formAdd =document.querySelector(".js-btn-add");
+
+
+menuAdd.addEventListener("click",(event)=>{
+    event.preventDefault();
+    const valueinput=(input =js-btn-add .value; formAdd.innerHTML=
+
+    console.log("add click");
+
+
+});
